53a7e50f3722f5e46455329f15c9684c770faf46
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-15 22:24:54 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,10 +19,11 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-
+  array.shift
 end
 
 def last_element_with_array_methods(array)
+
 end
 
 def length_of_array(array)
